18b5a8770e05c2926785df09ff86c63ba0e3bd31
lookaffe <lookaffe@gmail.com>
2019-08-28 14:41:06 +0200

    launcher

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+sleep 10
+cd /
+cd home/pi/Miac/
+#python3 tv_rot-MIAC-V1_2-rotary.py &
+#python3 radio_rot-MIAC-V1_0-rotary.py &
+cd /
